180847396b7f0d0d1b88bc13862f061bd624d78e
Alexey Lukin <alukin@qarea.us>
2014-11-04 10:05:27 -0500

    added less

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,11 @@ require.config({
 	baseUrl: 'js',
 	paths: {
 		jquery: 'vendor/jquery-1.10.2.min',
-		swig: 'vendor/swig.min'
+		swig: 'vendor/swig.min',
+		less: 'vendor/less.min'
 	}
 });
-
+require(['less']);
 require(['app'], function (app) {
 	app.init();
 });
